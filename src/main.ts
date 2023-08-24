7d7b6d5b0066a649081e7ada990f949f7bf3fcce
Nicholas Hobbs <nick@nhobb.com>
2023-08-24 21:04:33 +1000

    Use local file not download every time

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Jimp from 'jimp';
 
-const file = await Jimp.read('https://upload.wikimedia.org/wikipedia/en/4/42/Richard_Feynman_Nobel.jpg');
+const file = await Jimp.read('data/feynman.jpg');
 const settings = { width: 1920, height: 1080 };
 
 const result = file.contain(settings.width, settings.height)
