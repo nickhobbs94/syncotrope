8364c26ee3328c79edaea64440fb7e8d399fb555
Nicholas Hobbs <nick@nhobb.com>
2023-08-24 19:17:52 +1000

    Add prettier

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,18 +1,18 @@
-import { describe, it } from 'node:test';
-import assert from 'node:assert';
+import { describe, it } from "node:test";
+import assert from "node:assert";
 
-describe('A thing', () => {
-    it('should work', () => {
-        assert.strictEqual(1, 1);
-    });
+describe("A thing", () => {
+  it("should work", () => {
+    assert.strictEqual(1, 1);
+  });
 
-    it('should be ok', () => {
-        assert.strictEqual(2, 2);
-    });
+  it("should be ok", () => {
+    assert.strictEqual(2, 2);
+  });
 
-    describe('a nested thing', () => {
-        it('should work', () => {
-        assert.strictEqual(3, 3);
-        });
+  describe("a nested thing", () => {
+    it("should work", () => {
+      assert.strictEqual(3, 3);
     });
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 type Options = {
   width: number;
   height: number;
-}
+};
 
 function setDefaults(given: Partial<Options>) {
   return {
     width: given.width ?? 1920,
     height: given.height ?? 1080,
-  }
+  };
 }
 
-console.log(setDefaults({width: 1}));
+console.log(setDefaults({ width: 1 }));
