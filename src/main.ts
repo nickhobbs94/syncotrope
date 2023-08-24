a26330c618c626399d2578131b81207dd6b124a9
Nicholas Hobbs <nick@nhobb.com>
2023-08-24 17:53:47 +1000

    Set up basic typescript project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+type Options = {
+  width: number;
+  height: number;
+}
+
+
+function setDefaults(given: Partial<Options>) {
+  return {
+    width: given.width ?? 1920,
+    height: given.height ?? 1080,
+  }
+}
+
+console.log(setDefaults({width: 1}));
+
