0852232f8a62a9c5392f927c949f5cda2df5f85b
Nicholas Hobbs <nick@nhobb.com>
2023-08-25 20:43:23 +1000

    Get webpack working

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import Jimp from 'jimp';
-
-const file = await Jimp.read('data/feynman.jpg');
-const settings = { width: 1920, height: 1080 };
-
-const result = file.contain(settings.width, settings.height)
-  .background(0xaaaaaaff)
-  .greyscale()
-  .write('data/output.jpg');
